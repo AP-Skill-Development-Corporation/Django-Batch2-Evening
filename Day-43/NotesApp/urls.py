a7f5a4be5a014dab33b794ee5e85174eba0ff38f
EVNG <rravikumar34@gmail.com>
2023-08-23 20:23:14 +0530

    Day-43 Updated

---
+++ Day-43/NotesApp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from NotesApp import views
+from django.contrib.auth import views as v
+
+urlpatterns = [
+	path('',views.home,name="hm"),
+	path('abt/',views.about,name="ab"),
+	path('cnt/',views.contact,name="ct"),
+	path('reg/',views.register,name="rg"),
+	path('login/',v.LoginView.as_view(template_name="notehtmls/login.html"),name="lg"),
+	path('logout/',v.LogoutView.as_view(template_name="notehtmls/logout.html"),name="lgo"),
+	path('roles/',views.rolechange,name="role"),
+	path('roleup/<int:t>/',views.roleupdate,name="rolup"),
+	path('pfle/',views.profile,name="pf"),
+	path('uppf/',views.updateprofile,name="uppfle"),
+]
